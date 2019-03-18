02334000aaf177fe4ca939225735239cd8b1fff0
Martin <martin.srsen@fer.hr>
2019-03-18 11:11:11 +0100

    Second homework

---
+++ hw02-0036502135/src/main/java/hr/fer/zemris/java/custom/collections/demo/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Package contains program that demonstrates simple use of ObjectStack.
+ * Created for 2nd homework for OPJJ fer course.
+ */
+
+package hr.fer.zemris.java.custom.collections.demo;
